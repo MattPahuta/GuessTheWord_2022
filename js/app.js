c63362addee0ce68e0ed12bb932d8d662a37e406
Matt Pahuta <mhpahuta@gmail.com>
2022-10-22 13:50:21 -0700

    Add initial markup, styles, and js variables

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Game variables - global
+// *** move these into functions where possible
+
+const guessedLettersList = document.getElementById('guessed-letters-list');
+const letterInput = document.getElementById('letter');
+const wordInProgress = document.getElementById('word-in-progress');
+const remainingGuessesP = document.getElementById('remaining');
+const remainingGuessesSpan = document.getElementById('remaining-guesses');
+const message = document.getElementById('message');
+const guessBtn = document.getElementById('guess-btn');
+const playAgainBtn = document.getElementById('play-again-btn');
